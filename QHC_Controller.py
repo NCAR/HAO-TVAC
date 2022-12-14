1334ed9043191c43318a64d52b1e02ad6d948230
mitchem890 <mitchem890@gmail.com>
2022-12-14 12:09:39 -0700

    Rework of QHC

--- QHC_Controller.py
+++ QHC_Controller.py
@@ -25,70 +25,106 @@ class QHC_Controller:
     def Get_RAW(self):
         command = "RAW"
         return self.Send_Command(command)
-    #Todo This will lower the kp value but we should also increase the kp to the allowed limit as we approach the Target Temp
+
+
+    #This is a convoluted function Could use Clarity
+    #Checks to see if we want to limit duty Cycle
+    #If so is there is no value assignged to the Currentl temperature we cant do any thing
     def Check_KP_Good(self, channel, temp=None):
         if self.limit_duty_cycle:
             if self.Channels[channel-1].temp_current is None:
+                #No Value assigned to the Current Temperature
+                #We cant do anything
                 return
             if temp is None:
+                #if no temp is given we will use the current target temp
                 temp = self.Channels[channel-1].temp_target.strip('C')
+                
                 if temp == "error" or temp is None:
+                #If we got an error
+                #Return
                     return
                 else:
+                    #Conver the temp to a float
                     temp = float(temp)
+
+            #Check to see if the KP value will work currently
             if (temp - float(self.Channels[channel-1].temp_current.strip('C')))*float(self.Channels[channel-1].kp) != self.max_duty_cycle:
+                #Set the New Value so that the duty cycle is the max
                 kp = self.max_duty_cycle/(temp - float(self.Channels[channel-1].temp_current.strip('C')))
-                print(f"Warning: The KP value for channel {channel} is has changed. Setting KP to {kp}")
+                print(f"Warning: The KP value for channel {channel} is has changed. Setting KP to {kp} to maintain a duty cycle of {self.max_duty_cycle}")
                 self.Set_KP(channel, kp)
 
-    def Set_Temp(self, channel, temp):
-        self.Check_KP_Good(channel, temp)
-
-        command = f"C{channel}"
-        self.Send_Command(command)
-        command = f"T{temp}"
-        self.Send_Command(command)
-
     
-    def Set_KP(self, channel, kp):
+    #Select a Channel
+    def Select_Channel(self, channel):
         command = f"C{channel}"
         self.Send_Command(command)
-        command = f"p{kp}"
-        self.Send_Command(command)
-
-    def Set_KI(self, channel, ki):
-        command = f"C{channel}"
-        self.Send_Command(command)
-        command = f"i{ki}"
-        self.Send_Command(command)
-
-    def Set_enable(self, channel, enable):
-        if enable:
-            command = f"C{channel}"
-            self.Send_Command(command)
-            command = f"e"
-            self.Send_Command(command)
-        else:
-            command = f"C{channel}"
-            self.Send_Command(command)
-            command = f"d"
-            self.Send_Command(command)
+    
+    #Set a new Temp and Check if we need to update the KP value
+    def Set_Temp(self, channel, temp):
+        self.Select_Channel(channel)
+        self.Check_KP_Good(channel, temp)
+        self.Send_Command(f"t{temp}")
         
+    #Set the Frequency of the PWM
+    def Set_Frequency(self, channel, frequency):
+        self.Select_Channel(channel)
+        self.Send_Command(f"freq={frequency}")
+
+    #Set the proportional gain
+    def Set_KP(self, channel, kp):
+        self.Select_Channel(channel)
+        self.Send_Command(f"p{kp}")
+
+    #Set the Integral Gain
+    def Set_KI(self, channel, ki):
+        self.Select_Channel(channel)
+        self.Send_Command(f"i{ki}")
+    
+    #Set the Derivative Gain
+    def Set_KD(self, channel, kd):
+        self.Select_Channel(channel)
+        self.Send_Command(f"d{kd}")
+    
+    def Set_Enable(self, channel, enable):
+        self.Select_Channel(channel)
+        if enable:
+            self.Send_Command(f"e")
+        else:
+            self.Send_Command(f"d")
+    
+    #Set the limit of; the I term
+    def Set_LI(self, channel, li):
+        self.Select_Channel(channel)
+        self.Send_Command(f"I{li}")
+    
+    #Set the Address to Check for temperature at
     def Set_Address(self, channel, address):
-        command = f"C{channel}"
-        self.Send_Command(command)
-        command = f"A{address}"
-        self.Send_Command(command)
+        self.Select_Channel(channel)
+        self.Send_Command(f"a{address}")
+
+    #Set the History length for the integral term
+    def Set_History(self, channel, history):
+        self.Select_Channel(channel)
+        self.Send_Command(f"h{history}")
+
+    #Save the Current Settings to the EEPROM
+    def Save_Settings(self, channel):
+        self.Select_Channel(channel)
+        self.Send_Command(f"s")
+    
+    def Reset(self):
+        self.Send_Command(f"Bounce")
 
     #Send a Command to the Controller
     def Send_Command(self, command):
         self.ser.write(f"{command}\n".encode())
-    
         time.sleep(0.5)
         data = self.ser.read_all().decode('utf-8')
         return data
 
- #read in the Raw data and place it into a pandas table
+ #read in the Raw data and place it into a pandas table to be parsed later
     def Read_Raw(self):
         #Header on the First row
         #remove all threpeated spaces from the string
@@ -175,6 +211,19 @@ class QHC_Channel:
 if __name__ == "__main__":
     port = input("Enter the Port: ")
     controller = QHC_Controller(port)
+    #Request user input for which channel to use
+    channel = int(input("Enter the Channel [1,2,3,4]: "))
+    #Request user to input a Set Temp for the Channel
+    temp = int(input("Enter Desired Temperature: "))
+    #Ask user is they want to limit the Duty Cycle
+    limit = input("Limit Duty Cycle? (y/n): ")
+    if limit == "Y" or limit == "y":
+        #Request user to input a Duty Cycle Limit
+        limit = -1
+        while limit <=0:
+            limit = float(input("Enter Duty Cycle Limit [0-1]: "))
+        controller.max_duty_cycle = limit
+    
     controller.Update_Channels()
     #Set the Temperature Target
     controller.Set_KP(2, 1)
